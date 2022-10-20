fe350a691219c76eb20c72cecd5c777c915cddc2
Ogrimor <ogrimors37@gmail.com>
2022-10-20 15:38:15 +0500

    Cube Numbers

---
+++ cube_numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+if (x > 0)
+{
+    for (int i = 1; i < x + 1; i++)
+    {
+        if (i == x) 
+        {
+        Console.WriteLine(Math.Pow(i, 3));
+        break;
+        }
+        Console.Write(Math.Pow(i, 3) + ",");
+    }
+}
+else if (x < 0)
+{
+    for (int i = -1; i > x - 1; i--)
+    {
+        if (i == x)
+        {
+            Console.WriteLine(Math.Pow(i, 3));
+            break;
+        }
+        Console.Write(Math.Pow(i, 3) + ",");
+    }
+}
+else Console.WriteLine("Error");
